294135cbe2a4f457eb0da5ae1895425634c4467a
彭超 <3402977740@qq.com>
2023-11-12 08:29:33 +0800

    联动更改

--- gaming/__init__.py
+++ gaming/__init__.py
@@ -1,5 +1,6 @@
 from . import events
 from . import entity
+from . import setting
 from . import settings
 from . import event
 from . import scene
--- test.py
+++ test.py
@@ -5,18 +5,42 @@ import random
 import pygame as pg
 from OpenGL.GL import *
 import numpy as np
+import math
 
 def __import():
     global Game
     from game import Game
 
+def gradient(A, B, Step):
+    return [
+        (
+            A[0] + (B[0] - A[0]) / Step * N,
+            A[1] + (B[1] - A[1]) / Step * N,
+            A[2] + (B[2] - A[2]) / Step * N
+        )
+        for N in range(Step + 1)
+    ]
+
 def test(game: 'Game') -> Scene:
     cubes: list[Cube] = []
-
-    for i in range(1000):
-        cube = Cube(*[random.random() * 10 for _ in range(3)], colors=[random.random() for _ in range(3)])
+    mm = 100
+    ml = mm * 7
+    grad = [
+        *gradient((0, 0, 0), (0, 0, 255), mm),
+        *gradient((0, 0, 255), (0, 255, 0), mm),
+        *gradient((0, 255, 0), (0, 255, 255), mm),
+        *gradient((0, 255, 255), (255, 0, 0), mm),
+        *gradient((255, 0, 0), (255, 0, 255), mm),
+        *gradient((255, 0, 255), (255, 255, 0), mm),
+        *gradient((255, 255, 0), (255, 255, 255), mm)
+    ]
+    for i in range(ml):
+        
+        cube = Cube(*[10 for _ in range(3)], colors=[grad[i][0]/ 255,grad[i][1]/ 255,grad[i][2]/ 255])
         cubes.append(cube)
-    for cube in cubes:
-        cube.move(*np.array([random.random()* 1000 * random.randrange(-1, 2, 2) for _ in range(3)]))
-        cube.rotate(*[random.randrange(0, 361) for _ in range(3)])
+    for i, cube in enumerate(cubes):
+        cube.move(*np.array([
+            i * 10, i * 10, i * 10
+        ]))
+        # cube.rotate(*[random.randrange(0, 361) for _ in range(3)])
     return Scene(cubes)
