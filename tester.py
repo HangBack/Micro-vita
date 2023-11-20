a85323f3e10ef3fe17c66a48cc73d61a4d8913bb
彭超 <3402977740@qq.com>
2023-11-20 12:42:33 +0800

    数据结构-树测试

--- tester.py
+++ tester.py
@@ -1,13 +1,28 @@
-from typing import Any
+from engine.tools.orgnizations import Node, Tree
+import time
+
+root = Node(0)
+a = Node(123)
+b = Node(456)
+c = Node(789)
+d = Node(111)
+e = Node(222)
+f = Node(333)
+
+b.parent = a
+a.parent = c
+c.parent = b
+
+# a -> b
+# |    |
+# |----c
+
+c.parent = root
+
+tree = Tree(root, '你好')
 
 
-class obj:
-
-    def __init__(self) -> None:
-        self.__list = [0, 1, 2, 3]
-
-    def __getitem__(self, index):
-        return self.__list[index]
-        pass
-
-print(obj()[1])
+s = time.time()
+for _ in range(100000):
+    tree.BFS(callback=lambda x: x.data == 1)
+print(time.time() - s)
